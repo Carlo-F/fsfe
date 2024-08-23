878874f32a87aef0ae02fe5370c4e41b5a76de1e
Carlo Feniello <carlofeniello@hotmail.it>
2024-08-23 14:28:16 +0200

    add webSocket

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,27 @@ app.get('/', function(req, res) {
 server.on('request', app);
 
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+
+/** Begin websocket */
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({server: server});
+
+wss.on('connection', function connection(ws) {
+	const numClients = wss.clients.size;
+	console.log('Clients connected', numClients);
+
+	wss.broadcast(`Current visitors ${numClients}`);
+
+	if (ws.readyState === ws.OPEN) {
+		ws.send('Welcome to my server');
+	}
+
+	ws.on('close', function close() {
+		console.log('A client has disconnected');
+	});
+});
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(client => client.send(data));
+};
